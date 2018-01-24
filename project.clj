ebb244e333e9ea2fca2e1d788b0446a4455bc20d
Oskar Zabik <oskar.zabik@gmail.com>
2018-01-24 14:30:49 +0100

    add project url and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kauk "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A bunch of test and date utilities for Clojure(Script) projects."
+  :url "https://github.com/RackSec/kauk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
